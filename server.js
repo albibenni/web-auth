da8dedeb966c9ad536bcc5649710af434c512d5a
Alberto Benatti <albi.benni8@gmail.com>
2024-04-26 21:25:39 +0200

    feat: register and login with salt and hash

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express'
 import { Low } from 'lowdb'
 import { JSONFile } from 'lowdb/node'
 import * as url from 'url';
+import bcrypt from 'bcryptjs';
+import * as jwtJsDecode from 'jwt-js-decode';
+import base64url from "base64url";
+import SimpleWebAuthnServer from '@simplewebauthn/server';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -36,3 +40,45 @@ app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 });
 
+app.post("/auth/login", (req, res) => {
+    const user = findUser(req.body.email);
+    if (user) {
+        // user exists, check password
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+            res.send({ok: true, email: user.email, name: user.name});
+        } else {
+            res.send({ok: false, message: 'Data is invalid'});            
+        }
+    } else {
+        // User doesn't exist
+        res.send({ok: false, message: 'Data is invalid'});
+    }
+}); 
+
+function findUser(email) {
+    const results = db.data.users.filter(u=>u.email==email);
+    if (results.length==0) return undefined;
+    return results[0];
+}
+
+app.post("/auth/register", (req, res) => {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
+
+    const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+    };
+    const userFound = findUser(req.body.email);
+
+    if (userFound) {
+        // User already registered
+        res.send({ok: false, message: 'User already exists'});
+    } else {
+        // New User
+        db.data.users.push(user);
+        db.write();
+        res.send({ok: true});
+    }
+});
