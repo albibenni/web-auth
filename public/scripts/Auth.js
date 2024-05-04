ad16f2667d483e8c13bf7de632e398188ead3db3
Alberto Benatti <albi.benni8@gmail.com>
2024-05-04 20:57:37 +0200

    feat: authm button and fix cdn failing on v10.0

--- public/scripts/Auth.js
+++ public/scripts/Auth.js
@@ -2,130 +2,144 @@ import API from "./API.js";
 import Router from "./Router.js";
 
 const Auth = {
-    isLoggedIn: false,
-    account: null,
-    postLogin: (response, user) => {
-        if (response.ok) {
-            Auth.isLoggedIn = true;
-            Auth.account = user;
-            Auth.updateStatus();
-            Router.go("/account");
-        } else {
-            alert(response.message); // usually ui lib would be used, not base alert
-        }
-        // Credential Management API storage
-        if (window.PasswordCredential && user.password) {
-            const credential = new PasswordCredential({ id: user.email, password: user.password, name: user.name });
-            navigator.credentials.store(credential);
-            try {
-                navigator.credentials.store(credential);
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    },
-    loginFromGoogle: async (data) => {
-        const response = await API.loginFromGoogle({ credential: data.credential });
-        Auth.postLogin(response,
-            {
-                name: response.name,
-                email: response.email
-            });
+  isLoggedIn: false,
+  account: null,
+  postLogin: (response, user) => {
+    if (response.ok) {
+      Auth.isLoggedIn = true;
+      Auth.account = user;
+      Auth.updateStatus();
+      Router.go("/account");
+    } else {
+      alert(response.message); // usually ui lib would be used, not base alert
+    }
+    // Credential Management API storage
+    if (window.PasswordCredential && user.password) {
+      const credential = new PasswordCredential({
+        id: user.email,
+        password: user.password,
+        name: user.name,
+      });
+      navigator.credentials.store(credential);
+      try {
+        navigator.credentials.store(credential);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
+  loginFromGoogle: async (data) => {
+    const response = await API.loginFromGoogle({ credential: data.credential });
+    Auth.postLogin(response, {
+      name: response.name,
+      email: response.email,
+    });
+  },
+  register: async (event) => {
+    event.preventDefault();
+    const user = {
+      name: document.getElementById("register_name").value,
+      email: document.getElementById("register_email").value,
+      password: document.getElementById("register_password").value,
+    };
+    const response = await API.register(user);
+    Auth.postLogin(response, user);
+  },
+  checkAuthOptions: async () => {
+    const response = await API.checkAuthOptions({
+      email: document.getElementById("login_email").value,
+    });
+    Auth.loginStep = 2;
+    if (response.password) {
+      document.getElementById("login_section_password").hidden = false;
+    }
+    if (response.webauthn) {
+      document.getElementById("login_section_webauthn").hidden = false;
+    }
+  },
+  addWebAuthn: async () => {
+    const options = await API.webAuthn.registrationOptions();
+    options.authenticatorSelection.residentKey = "required";
+    options.authenticatorSelection.requireResidentKey = true;
+    options.extensions = {
+      credProps: true,
+    };
 
-    },
-    register: async (event) => {
-        event.preventDefault();
-        const user = {
-            name: document.getElementById("register_name").value,
-            email: document.getElementById("register_email").value,
-            password: document.getElementById("register_password").value
-        };
-        const response = await API.register(user);
-        Auth.postLogin(response, user);
-    },
-    checkAuthOptions: async () => {
-        const response = await API.checkAuthOptions({
-            email: document.getElementById("login_email").value
-        });
-        Auth.loginStep = 2;
-        if (response.password) {
-            document.getElementById("login_section_password").hidden = false;
-        }
-        if (response.webauthn) {
+    const authRes = await SimpleWebAuthnBrowser.startRegistration(options);
+    const verificationRes = await API.webAuthn.registrationVerification(
+      authRes,
+    );
+    if (verificationRes.ok) {
+      alert("You can now login with webAuthn");
+    } else {
+      alert(verificationRes.message);
+    }
+  },
+  login: async (event) => {
+    event?.preventDefault();
 
-            document.getElementById("login_section_webauthn").hidden = false;
-        }
-
-    },
-    login: async (event) => {
-        event?.preventDefault();
-
-        if (Auth.loginStep == 1) {
-
-            Auth.checkAuthOptions();
-        } else {
-
-            const credentials = {
-                email: document.getElementById("login_email").value,
-                password: document.getElementById("login_password").value
-            };
-            const response = await API.login(credentials);
-            Auth.postLogin(response, {
-                email: response.email,
-                name: response.name
-            });
-        }
-    },
-    autlogin: async () => {
-        if (window.PasswordCredential) {
-            const credential = await navigator.credentials.get({ password: true });
-            if (credential) {
-                document.getElementById("login_email").value = credential.id;
-                document.getElementById("login_password").value = credential.password;
-                Auth.login();
-            }
-        }
-    },
-    logout: () => {
-        Auth.isLoggedIn = false;
-        Auth.account = null;
-        Auth.updateStatus();
-        Router.go("/");
-        if (window.PasswordCredential) {
-            navigator.credentials.preventSilentAccess();
-        }
-    },
-    updateStatus() {
-        if (Auth.isLoggedIn && Auth.account) {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "none"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".account_name").forEach(
-                e => e.innerHTML = Auth.account.name
-            );
-            document.querySelectorAll(".account_username").forEach(
-                e => e.innerHTML = Auth.account.email
-            );
-
-        } else {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "none"
-            );
-
-        }
-    },
-    loginStep: 1,
-    init: () => {
-        document.getElementById("login_section_password").hidden = true;
-        document.getElementById("login_section_webauthn").hidden = true;
-    },
-}
+    if (Auth.loginStep == 1) {
+      Auth.checkAuthOptions();
+    } else {
+      const credentials = {
+        email: document.getElementById("login_email").value,
+        password: document.getElementById("login_password").value,
+      };
+      const response = await API.login(credentials);
+      Auth.postLogin(response, {
+        email: response.email,
+        name: response.name,
+      });
+    }
+  },
+  autlogin: async () => {
+    if (window.PasswordCredential) {
+      const credential = await navigator.credentials.get({ password: true });
+      if (credential) {
+        document.getElementById("login_email").value = credential.id;
+        document.getElementById("login_password").value = credential.password;
+        Auth.login();
+      }
+    }
+  },
+  logout: () => {
+    Auth.isLoggedIn = false;
+    Auth.account = null;
+    Auth.updateStatus();
+    Router.go("/");
+    if (window.PasswordCredential) {
+      navigator.credentials.preventSilentAccess();
+    }
+  },
+  updateStatus() {
+    if (Auth.isLoggedIn && Auth.account) {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "none"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".account_name")
+        .forEach((e) => (e.innerHTML = Auth.account.name));
+      document
+        .querySelectorAll(".account_username")
+        .forEach((e) => (e.innerHTML = Auth.account.email));
+    } else {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "none"));
+    }
+  },
+  loginStep: 1,
+  init: () => {
+    document.getElementById("login_section_password").hidden = true;
+    document.getElementById("login_section_webauthn").hidden = true;
+  },
+};
 Auth.updateStatus();
 Auth.autlogin();
 
--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-import express from 'express'
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
-import * as url from 'url';
-import bcrypt from 'bcryptjs';
-import * as jwtJsDecode from 'jwt-js-decode';
+import express from "express";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+import * as url from "url";
+import bcrypt from "bcryptjs";
+import * as jwtJsDecode from "jwt-js-decode";
 import base64url from "base64url";
-import SimpleWebAuthnServer from '@simplewebauthn/server';
-import { truncate } from 'fs';
-import { request } from 'http';
+import SimpleWebAuthnServer from "@simplewebauthn/server";
+import { truncate } from "fs";
+import { request } from "http";
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const adapter = new JSONFile(__dirname + '/auth.json');
+const adapter = new JSONFile(__dirname + "/auth.json");
 const db = new Low(adapter);
 await db.read();
-db.data ||= { users: [] }
+db.data ||= { users: [] };
 
 const rpID = "localhost";
 const protocol = "http";
 const port = 5050;
 const expectedOrigin = `${protocol}://${rpID}:${port}`;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 
 // ADD HERE THE REST OF THE ENDPOINTS
 
@@ -37,7 +39,7 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+    console.log(`App listening on port ${port}`);
 });
 
 app.post("/auth/auth-options", (req, res) => {
@@ -46,11 +48,11 @@ app.post("/auth/auth-options", (req, res) => {
         res.send({
             password: userFound.password != false,
             google: userFound.federated && userFound.federated.google,
-            webauthn: userFound.webauthn
+            webauthn: userFound.webauthn,
         });
     } else {
         res.send({
-            password: true
+            password: true,
         });
     }
 });
@@ -60,12 +62,12 @@ app.post("/auth/login-google", (req, res) => {
     const user = {
         email: jwt.payload.email,
         name: jwt.payload.given_name + " " + jwt.payload.family_name,
-        password: undefined
+        password: undefined,
     };
     const userFound = findUser(user.email);
     if (userFound) {
         user.federated = {
-            google: jwt.payload.aud
+            google: jwt.payload.aud,
         };
         db.write();
         res.send({ ok: true, name: user.name, email: user.email });
@@ -73,13 +75,12 @@ app.post("/auth/login-google", (req, res) => {
         db.data.users.push({
             ...user,
             federated: {
-                google: jwt.payload.aud
-            }
-        })
+                google: jwt.payload.aud,
+            },
+        });
         db.write();
         res.send({ ok: true, name: user.name, email: user.email });
     }
-
 });
 
 app.post("/auth/login", (req, res) => {
@@ -89,16 +90,16 @@ app.post("/auth/login", (req, res) => {
         if (bcrypt.compareSync(req.body.password, user.password)) {
             res.send({ ok: true, email: user.email, name: user.name });
         } else {
-            res.send({ ok: false, message: 'Credentials are wrong.' }); // Credentials are wrong is a standard to avoid giving hakers information about what is wrong
+            res.send({ ok: false, message: "Credentials are wrong." }); // Credentials are wrong is a standard to avoid giving hackers information about what is wrong
         }
     } else {
         // User doesn't exist
-        res.send({ ok: false, message: 'Credentials are wrong.' }); // Credentials are wrong is a standard to avoid giving hakers information about what is wrong
+        res.send({ ok: false, message: "Credentials are wrong." }); // Credentials are wrong is a standard to avoid giving hackers information about what is wrong
     }
 });
 
 function findUser(email) {
-    const results = db.data.users.filter(u => u.email == email);
+    const results = db.data.users.filter((u) => u.email == email);
     if (results.length == 0) return undefined;
     return results[0];
 }
@@ -111,13 +112,13 @@ app.post("/auth/register", (req, res) => {
     const user = {
         name: req.body.name,
         email: req.body.email,
-        password: hash
+        password: hash,
     };
     const userFound = findUser(req.body.email);
 
     if (userFound) {
         // User already registered
-        res.send({ ok: false, message: 'User already exists' });
+        res.send({ ok: false, message: "User already exists" });
     } else {
         // New User
         db.data.users.push(user);
@@ -133,12 +134,12 @@ app.post("/auth/webauth-registration-options", (req, res) => {
     const user = findUser(req.body.email);
 
     const options = {
-        rpName: 'Web Auth', // name of the relying party -- look at WebAuth - https://github.com/albibenni/ObsidianVault/blob/master/Computer%20Science/Security/Autentication/WebAuthn%20-%20Passkey.md
+        rpName: "Web Auth", // name of the relying party -- look at WebAuth - https://github.com/albibenni/ObsidianVault/blob/master/Computer%20Science/Security/Autentication/WebAuthn%20-%20Passkey.md
         rpID, // relying part id - domain - defined down
         userID: user.email,
         userName: user.name,
         timeout: 60000, // timeout for user auth
-        attestationType: 'none',
+        attestationType: "none",
 
         /**
          * Passing in a user's list of already-registered authenticator IDs here prevents users from
@@ -146,15 +147,17 @@ app.post("/auth/webauth-registration-options", (req, res) => {
          * the browser if it's asked to perform registration when one of these ID's already resides
          * on it.
          */
-        excludeCredentials: user.devices ? user.devices.map(dev => ({
-            id: dev.credentialID,
-            type: 'public-key',
-            transports: dev.transports,
-        })) : [],
+        excludeCredentials: user.devices
+            ? user.devices.map((dev) => ({
+                id: dev.credentialID,
+                type: "public-key",
+                transports: dev.transports,
+            }))
+            : [],
 
         authenticatorSelection: {
-            userVerification: 'required',
-            residentKey: 'required',
+            userVerification: "required",
+            residentKey: "required",
         },
         /**
          * The two most common algorithms: ES256, and RS256
@@ -166,7 +169,7 @@ app.post("/auth/webauth-registration-options", (req, res) => {
      * The server needs to temporarily remember this value for verification, so don't lose it until
      * after you verify an authenticator response.
      */
-    const regOptions = SimpleWebAuthnServer.generateRegistrationOptions(options)
+    const regOptions = SimpleWebAuthnServer.generateRegistrationOptions(options);
     user.currentChallenge = regOptions.challenge;
     db.write();
 
@@ -188,7 +191,9 @@ app.post("/auth/webauth-registration-verification", async (req, res) => {
             expectedRPID: rpID,
             requireUserVerification: true,
         };
-        verification = await SimpleWebAuthnServer.verifyRegistrationResponse(options);
+        verification = await SimpleWebAuthnServer.verifyRegistrationResponse(
+            options,
+        );
     } catch (error) {
         console.log(error);
         return res.status(400).send({ error: error.toString() });
@@ -199,9 +204,11 @@ app.post("/auth/webauth-registration-verification", async (req, res) => {
     if (verified && registrationInfo) {
         const { credentialPublicKey, credentialID, counter } = registrationInfo;
 
-        const existingDevice = user.devices ? user.devices.find(
-            device => new Buffer.from(device.credentialID.data).equals(credentialID)
-        ) : false;
+        const existingDevice = user.devices
+            ? user.devices.find((device) =>
+                new Buffer.from(device.credentialID.data).equals(credentialID),
+            )
+            : false;
 
         if (!existingDevice) {
             const newDevice = {
@@ -222,7 +229,7 @@ app.post("/auth/webauth-registration-verification", async (req, res) => {
 });
 
 //Login webauthn
-app.post("/auth/webauth-login-options", (req, res) =>{
+app.post("/auth/webauth-login-options", (req, res) => {
     const user = findUser(req.body.email);
     // if (user==null) {
     //     res.sendStatus(404);
@@ -231,12 +238,15 @@ app.post("/auth/webauth-login-options", (req, res) =>{
     const options = {
         timeout: 60000,
         allowCredentials: [],
-        devices: user && user.devices ? user.devices.map(dev => ({
-          id: dev.credentialID,
-          type: 'public-key',
-          transports: dev.transports,
-        })) : [],
-        userVerification: 'required',
+        devices:
+            user && user.devices
+                ? user.devices.map((dev) => ({
+                    id: dev.credentialID,
+                    type: "public-key",
+                    transports: dev.transports,
+                }))
+                : [],
+        userVerification: "required",
         rpID,
     };
     const loginOpts = SimpleWebAuthnServer.generateAuthenticationOptions(options);
@@ -247,57 +257,66 @@ app.post("/auth/webauth-login-options", (req, res) =>{
 app.post("/auth/webauth-login-verification", async (req, res) => {
     const data = req.body.data;
     const user = findUser(req.body.email);
-    if (user==null) {
-        res.sendStatus(400).send({ok: false});
+    if (user == null) {
+        res.sendStatus(400).send({ ok: false });
         return;
-    } 
-  
+    }
+
     const expectedChallenge = user.currentChallenge;
-  
+
     let dbAuthenticator;
     const bodyCredIDBuffer = base64url.toBuffer(data.rawId);
 
     for (const dev of user.devices) {
-      const currentCredential = Buffer(dev.credentialID.data);
-      if (bodyCredIDBuffer.equals(currentCredential)) {
-        dbAuthenticator = dev;
-        break;
-      }
+        const currentCredential = Buffer(dev.credentialID.data);
+        if (bodyCredIDBuffer.equals(currentCredential)) {
+            dbAuthenticator = dev;
+            break;
+        }
     }
 
     if (!dbAuthenticator) {
-      return res.status(400).send({ ok: false, message: 'Authenticator is not registered with this site' });
+        return res
+            .status(400)
+            .send({
+                ok: false,
+                message: "Authenticator is not registered with this site",
+            });
     }
-  
+
     let verification;
     try {
-      const options  = {
-        credential: data,
-        expectedChallenge: `${expectedChallenge}`,
-        expectedOrigin,
-        expectedRPID: rpID,
-        authenticator: {
-            ...dbAuthenticator,
-            credentialPublicKey: new Buffer(dbAuthenticator.credentialPublicKey.data) // Re-convert to Buffer from JSON
-        },
-        requireUserVerification: true,
-      };
-      verification = await SimpleWebAuthnServer.verifyAuthenticationResponse(options);
+        const options = {
+            credential: data,
+            expectedChallenge: `${expectedChallenge}`,
+            expectedOrigin,
+            expectedRPID: rpID,
+            authenticator: {
+                ...dbAuthenticator,
+                credentialPublicKey: new Buffer(
+                    dbAuthenticator.credentialPublicKey.data,
+                ), // Re-convert to Buffer from JSON
+            },
+            requireUserVerification: true,
+        };
+        verification = await SimpleWebAuthnServer.verifyAuthenticationResponse(
+            options,
+        );
     } catch (error) {
-      return res.status(400).send({ ok: false, message: error.toString() });
+        return res.status(400).send({ ok: false, message: error.toString() });
     }
-  
+
     const { verified, authenticationInfo } = verification;
-  
+
     if (verified) {
-      dbAuthenticator.counter = authenticationInfo.newCounter;
+        dbAuthenticator.counter = authenticationInfo.newCounter;
     }
-  
-    res.send({ 
-        ok: true, 
+
+    res.send({
+        ok: true,
         user: {
-            name: user.name, 
-            email: user.email
-        }
+            name: user.name,
+            email: user.email,
+        },
     });
 });
